39efec2080795b8aab756f57fa0cf5054d9d964a
Tuan <1414369@hcmut.edu.vn>
2019-05-13 18:36:11 +0700

    init basic components and routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'order-success', component: OrderSuccessComponent },
+  { path: 'my/orders', component: MyOrdersComponent },
+  { path: 'check-out', component: CheckOutComponent },
+  { path: 'admin/products', component: AdminProductsComponent },
+  { path: 'admin/orders', component: AdminOrdersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
