14a8236687fe32fc4085870c25bd1105e616711c
Tuan <quangtuan9237@gmail.com>
2019-06-15 14:42:46 +0700

    redirect to no permission page when recieve 403 http error

--- src/app/_helper/server-error.interceptor.ts
+++ src/app/_helper/server-error.interceptor.ts
@@ -4,21 +4,29 @@ import {
     HttpEvent, HttpRequest, HttpHandler,
     HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, empty } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ServerErrorInterceptor implements HttpInterceptor {
+    constructor(private router: Router) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         return next.handle(request).pipe(
             retry(1),
             catchError((error: HttpErrorResponse) => {
-                if (error.status === 401) {
-                    // refresh token
-                } else {
-                    return throwError(error);
+                switch (error.status) {
+                    case 401:
+                        // refresh token
+                        break;
+                    case 403:
+                        this.router.navigateByUrl('/no-permission');
+                        return empty();
+                        break;
+                    default:
+                        return throwError(error);
                 }
             })
         );
